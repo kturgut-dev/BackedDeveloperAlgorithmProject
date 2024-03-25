80b8c89acd7f26dc0f75a39861bc774a525a999e
Kürşat Turgut <kursat.turgut@lefyapi.com.tr>
2024-03-26 02:39:33 +0300

    update ds

--- BackedDeveloperAlgorithmProject/Datasource/DurationBetweenLocationDatasource.cs
+++ BackedDeveloperAlgorithmProject/Datasource/DurationBetweenLocationDatasource.cs
@@ -7,6 +7,14 @@ public static class DurationBetweenLocationDatasource
     public static IEnumerable<DurationBetweenLocation>? GetDataSource()
     {
         // read wwwroot/duration_between_locations_minutes_matix.json file and return ds
-        return JsonHelper.ReadJsonFile<DurationBetweenLocation>($"wwwroot/duration_between_locations_minutes_matix.json") ?? new List<DurationBetweenLocation>();
+        return JsonHelper.ReadJsonFile<DurationBetweenLocation>($"wwwroot/duration_between_locations_minutes_matix.json") ?? new List<DurationBetweenLocation>
+        {
+            new DurationBetweenLocation { From = "A", To = "B", DurationMinutes = 15 },
+            new DurationBetweenLocation { From = "A", To = "C", DurationMinutes = 20 },
+            new DurationBetweenLocation { From = "A", To = "D", DurationMinutes = 10 },
+            new DurationBetweenLocation { From = "B", To = "C", DurationMinutes = 5 },
+            new DurationBetweenLocation { From = "B", To = "D", DurationMinutes = 25 },
+            new DurationBetweenLocation { From = "C", To = "D", DurationMinutes = 25 }
+        };
     }
 }
--- BackedDeveloperAlgorithmProject/Datasource/EventDatasource.cs
+++ BackedDeveloperAlgorithmProject/Datasource/EventDatasource.cs
@@ -7,6 +7,14 @@ public static class EventDatasource
     public static IEnumerable<Event>? GetDataSource()
     {
         // read wwwroot/event.json file and return ds
-        return JsonHelper.ReadJsonFile<Event>($"wwwroot/events.json") ?? new List<Event>();
+        return JsonHelper.ReadJsonFile<Event>($"wwwroot/events.json") ?? new List<Event>
+        {
+            new Event { Id = 1, StartTime = TimeSpan.Parse("10:00"), EndTime = TimeSpan.Parse("12:00"), Location = "A", Priority = 50 },
+            new Event { Id = 2, StartTime = TimeSpan.Parse("10:00"), EndTime = TimeSpan.Parse("11:00"), Location = "B", Priority = 30 },
+            new Event { Id = 3, StartTime = TimeSpan.Parse("11:30"), EndTime = TimeSpan.Parse("12:30"), Location = "A", Priority = 40 },
+            new Event { Id = 4, StartTime = TimeSpan.Parse("14:30"), EndTime = TimeSpan.Parse("16:00"), Location = "C", Priority = 70 },
+            new Event { Id = 5, StartTime = TimeSpan.Parse("14:25"), EndTime = TimeSpan.Parse("15:30"), Location = "B", Priority = 60 },
+            new Event { Id = 6, StartTime = TimeSpan.Parse("13:00"), EndTime = TimeSpan.Parse("14:00"), Location = "D", Priority = 80 }
+        };
     }
 }
